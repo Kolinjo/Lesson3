8535d17d392cf1b310d2f6e1437391c5d3c0ced1
Kolinjo <kozicmarko@yahoo.com>
2019-05-01 00:34:22 +0200

    SpeakAndSpell change

--- src/exercises/SpeakAndSpell.java
+++ src/exercises/SpeakAndSpell.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import javax.swing.JOptionPane;
+
 import voce.SpeechSynthesizer;
 
 /**
@@ -14,17 +16,76 @@ import voce.SpeechSynthesizer;
 public class SpeakAndSpell {
 
 	public static void main(String[] args) {
-		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
-		// 2. Catch the user's answer in a String
-
-		// 3. If the user spelled the word correctly, speak "correct"
-
-		// 4. Otherwise say "wrong"
-
-		// 5. Repeat the process for other words
 		
+		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
+		// 2. Catch the user's answer in a String
+		// 3. If the user spelled the word correctly, speak "correct"
+		// 4. Otherwise say "wrong"
+		// 5. Repeat the process for other word
 		// 6. Calculate the user's score and give it to them at the end of the game 
+		
+		//introducing score variable needed for calculating a score of positive spelling
+		int score = 0;
+		
+		//first word
+		speak("mandlebrot");
+				String answer = JOptionPane.showInputDialog("Spell the word correctly : ");
+				if (answer.equalsIgnoreCase("mandlebrot")) {
+					JOptionPane.showMessageDialog(null, "Bravo amigo, correctly spelt!");
+					score +=1;
+				}else {	
+					JOptionPane.showMessageDialog(null, "Wrongly spelt");
+					
+				}
+				System.out.println(score);
+			//second word	
+				speak("integrate");
+				String answer1 = JOptionPane.showInputDialog("Spell the word correctly : ");
+				
+				if (answer1.equalsIgnoreCase("integrate")) {
+					JOptionPane.showMessageDialog(null, "Bravo amigo, correctly spelt!");
+					score +=1;
+				}else {	
+					JOptionPane.showMessageDialog(null, "Wrongly spelt");
+					
+				}
+				System.out.println(score);
+			//third word	
+				speak("unimaginable");
+				String answer2 = JOptionPane.showInputDialog("Spell the word correctly : ");
+				
+				if (answer2.equalsIgnoreCase("unimaginable")) {
+					JOptionPane.showMessageDialog(null, "Bravo amigo, correctly spelt!");
+					score +=1;
+				}else {	
+					JOptionPane.showMessageDialog(null, "Wrongly spelt");
+					
+				}
+				System.out.println(score);
+			//forth word	
+				speak("recyclable");
+				String answer3 = JOptionPane.showInputDialog("Spell the word correctly : ");
+				
+				if (answer3.equalsIgnoreCase("recyclable")) {
+					JOptionPane.showMessageDialog(null, "Bravo amigo, correctly spelt!");
+					score +=1;
+				}else {	
+					JOptionPane.showMessageDialog(null, "Wrongly spelt");
+					
+				}
+				System.out.println(score);
+			//fifth word	
+				speak("biodegradable");
+				String answer4 = JOptionPane.showInputDialog("Spell the word correctly : ");
+				
+				if (answer4.equalsIgnoreCase("biodegradable")) {
+					JOptionPane.showMessageDialog(null, "Bravo amigo, correctly spelt!");
+					score +=1;
+				}else {	
+					JOptionPane.showMessageDialog(null, "Wrongly spelt");
+					
+				}
+				JOptionPane.showMessageDialog(null, "Total score or correct spelling is " + score);	
 		
 		
 	}
