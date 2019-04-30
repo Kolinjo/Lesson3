7f6ccae4dd1c99cac958a1708967879e8ccea781
Kolinjo <kozicmarko@yahoo.com>
2019-05-01 00:29:48 +0200

    HighLowGame change

--- src/exercises/HighLowGame.java
+++ src/exercises/HighLowGame.java
@@ -1,6 +1,36 @@
 package exercises;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class HighLowGame {
 
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		// introducing variable random number
+		int randomNumber = new Random().nextInt(100);
+		System.out.println(randomNumber);
+
+		System.out.println("Enter one number in range form 0 to 100.");
+		// going into for loop for 5 consecutive times
+		for (int i = 1; i <= 5; i++) {
+			// variable which store user's input
+			int enteredNumber = input.nextInt();
+			//going through if-else stage 
+			if (enteredNumber > randomNumber) {
+				System.out.println(
+						enteredNumber + " is higher than guessed number. You have " + (5 - i) + " tries more.");
+			} else if (enteredNumber < randomNumber) {
+				System.out.println(
+						enteredNumber + " is smaller than guessed number. You have " + (5 - i) + " tries more.");
+			} else {
+				System.out.println(enteredNumber + " is right number , great!!!.");
+			}
+		}
+		System.out.println("More luck next time!!");
+		input.close();
+
+	}
 
 }
